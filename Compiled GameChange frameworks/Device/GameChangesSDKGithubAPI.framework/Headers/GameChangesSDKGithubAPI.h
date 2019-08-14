151a6d4396d6150ffde07787b2edbf1dc260640b
Abhayam Rastogi <abhayam.rastogi@round.glass>
2019-08-15 02:55:56 +0530

    Added compiled frameworks for device & simualtor

---
+++ Compiled GameChange frameworks/Device/GameChangesSDKGithubAPI.framework/Headers/GameChangesSDKGithubAPI.h
@@ -0,0 +1,19 @@
+//
+//  GameChangesSDKGithubAPI.h
+//  GameChangesSDKGithubAPI
+//
+//  Created by Abhayam Rastogi on 12/08/19.
+//  Copyright © 2019 Abhayam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GameChangesSDKGithubAPI.
+FOUNDATION_EXPORT double GameChangesSDKGithubAPIVersionNumber;
+
+//! Project version string for GameChangesSDKGithubAPI.
+FOUNDATION_EXPORT const unsigned char GameChangesSDKGithubAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GameChangesSDKGithubAPI/PublicHeader.h>
+
+
---
+++ Compiled GameChange frameworks/Device/GameSDKCore.framework/Headers/GameSDKCore.h
@@ -0,0 +1,19 @@
+//
+//  GameSDKCore.h
+//  GameSDKCore
+//
+//  Created by Abhayam Rastogi on 12/08/19.
+//  Copyright © 2019 Abhayam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GameSDKCore.
+FOUNDATION_EXPORT double GameSDKCoreVersionNumber;
+
+//! Project version string for GameSDKCore.
+FOUNDATION_EXPORT const unsigned char GameSDKCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GameSDKCore/PublicHeader.h>
+
+
